6b619b13dfd6907f0cf0d6dc798e08c28066d7c7
Pity-0 <pityrl20@gmail.com>
2024-09-25 13:25:39 +0100

    Mi primer commit: primera version

---
+++ src/AppGrid.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import Grid  from '@mui/material/Grid2'
+
+
+
+export default function Appgrid(){
+
+ return (
+    
+<>   
+  <Grid size={{sm: 12, md:45}}>
+
+<Button variant='contained' color='secondary'>1</Button>
+<Button variant='contained' color='secondary'>2</Button>
+<Button variant='contained' color='secondary'>3</Button>
+
+
+  </Grid>  
+  
+  </>
+
+ )
+}
